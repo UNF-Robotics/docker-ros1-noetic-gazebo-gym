336d9c623a9a23d96474fcad0052c7e8e9eb13e3
William L. Thomson Jr. <w@wltjr.com>
2024-07-08 22:56:14 -0400

    build.sh: Build & install panda_gazebo
    - Packaged from https://github.com/rickstaa/panda-gazebo

--- build.sh
+++ build.sh
@@ -8,6 +8,20 @@ unzip -q noetic.zip
 mv -v ros-gazebo-gym-noetic ros-gazebo-gym
 rm -r noetic.zip
 
+wget -q https://github.com/rickstaa/panda-gazebo/archive/refs/heads/noetic.zip
+unzip -q noetic.zip
+mv -v panda-gazebo-noetic/panda_gazebo ./
+rm -r noetic.zip
+sed -i -e 's|^    gazebo|    GAZEBO|' \
+	-e '/dyn_reconf/d' \
+	panda_gazebo/CMakeLists.txt
+
+# install missing files
+echo 'install(
+    DIRECTORY cfg launch resources
+    DESTINATION share/${PROJECT_NAME}
+)' >> panda_gazebo/CMakeLists.txt
+
 # set ROS env
 . /opt/ros/noetic/setup.sh
 
@@ -25,3 +39,7 @@ rosdep install --from-paths src --ignore-src -r -y
 # build and install
 catkin config --install
 catkin build
+
+# modify franka_ros
+sed -i -e '20i\ \ <arg name="physics"     default="ode"   doc="" />' \
+        /opt/ros/noetic/share/franka_gazebo/launch/panda.launch
